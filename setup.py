3522276b755fb61acebee23c50f8813a7b5bc04a
Kye <kye@apacmediasolutions.com>
2023-07-15 16:25:21 -0400

    cybertron

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-  name = 'cybertron',
+  name = 'cybertron-llm',
   packages = find_packages(exclude=[]),
   version = '0.0.1',
   license='MIT',
